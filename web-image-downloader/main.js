780ab0813a5037b0f7ee0a9d2a7fd35a2056cca2
Leafurya <46297197+Leafurya@users.noreply.github.com>
2023-03-31 14:01:52 +0900

    Update main.js

--- web-image-downloader/main.js
+++ web-image-downloader/main.js
@@ -1,16 +1,13 @@
 const fs=require('fs')
-const file="img"
-// const urls=["https://i.pinimg.com/236x/bc/5d/f5/bc5df54b49b1611825dd554bac3e64d5.jpg","https://i.pinimg.com/564x/bc/5d/f5/bc5df54b49b1611825dd554bac3e64d5.jpg","https://i.pinimg.com/736x/bc/5d/f5/bc5df54b49b1611825dd554bac3e64d5.jpg"]
 
-const url=process.argv[2]
+const file=process.argv[2]
+const url=process.argv[3]
 
-// for(let i=0;i<urls.length;i++){
-// }
 fetch(url).then((res)=>{
     return res.blob()
 }).then((blob)=>{
     blob.arrayBuffer().then((data)=>{
         const view=new Uint8Array(data)
-        fs.writeFile(file+".jpg",view,(err)=>console.log(err))
+        fs.writeFile(file,view,(err)=>console.log(err))
     })
 })
